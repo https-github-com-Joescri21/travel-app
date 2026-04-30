c39db7f7ffa01019ac33f206f0722390a7f267a8
Joel Escribano Bonola <joescri21@gmail.com>
2026-04-30 11:21:13 -0600

    add: se cambia la paleta de colores y el home ademas de que se añade hashLocaionStrategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+
+     //HashLocationStrategy
+
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    }
+
   ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  weatherApiKey: '76c24507e1a72d630eb47cb94c3c88bf'
+  weatherApiKey: ''
 };
